0ab874c9cb72a2879c3f4b4ce736f08d8e7ea7be
francollovera <francollovera@gmail.com>
2023-08-11 23:53:54 -0300

    subido

---
+++ src/App.js
@@ -0,0 +1,52 @@
+
+// import './App.css';
+import Navbar from './components/Navbar';
+import { useEffect, useState } from 'react';
+import Characters from './components/Characters';
+import Pagination from './components/Pagination';
+
+
+
+function App() {
+  const [characters, setCharacters]= useState([]);
+  const [info, setInfo] = useState({});
+
+  const initialUrl = 'https://rickandmortyapi.com/api/character';
+  const fetchCharacters = (url) =>{
+      fetch(url)
+  .then(response => response.json())
+  .then(data => {
+   
+    setCharacters(data.results);
+    setInfo(data.info)
+  })
+  .catch(error => console.log('error'))
+  };
+  const onPrevius = () =>{
+    fetchCharacters(info.prev);
+  }
+  const onNext = () =>{
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+   fetchCharacters(initialUrl);
+  
+   
+  }, [])
+  
+  return (
+    <>
+    <Navbar brand= 'Rick and Morty' />  
+    <div className='container mt-5'>
+    <Pagination prev={info.prev} next={info.next} onPrevius={onPrevius} onNext={onNext}/> 
+    <div className='container mt-5'>
+      <Characters characters={characters} />
+      </div>    
+      <Pagination prev={info.prev} next={info.next} onPrevius={onPrevius} onNext={onNext}/> 
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Characters.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from './Characters.module.css'
+
+const Characters = ({characters = []}) => {
+  return (
+    <div className='row'>
+        
+            {
+                characters.map((item, index) => (
+                <div key={index} className='col mb-4'>
+                    <div className={style.card} >
+                        <img src={item.image} alt='' />
+                        <div className='card-body'>
+                        <h5 className='card-title' >{item.name}</h5>
+                        <hr />
+                        <p>species: {item.species}</p>
+                        <p>location: {item.location.name}</p>
+                       
+                        </div>
+                    </div>
+                </div>
+                ))
+            }
+
+       
+    </div>
+  )
+}
+
+export default Characters
